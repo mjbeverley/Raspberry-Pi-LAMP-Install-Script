db173eff07d6f9edbb1decad5bff3017788c72a0
mjbeverley <mjbeverley@gmail.com>
2013-08-10 18:28:50 +1000

    Update install.sh

--- install.sh
+++ install.sh
@@ -10,6 +10,9 @@
 #Prerequisites
 sudo apt-get update
 
+#Zeroconf/Bonjour
+sudo apt-get install -y avahi-daemon
+
 #FTP
 sudo apt-get install -y proftpd
 
@@ -18,7 +21,7 @@ sudo apt-get install -y apache2
 sudo echo "ServerName localhost" >> /etc/apache2/httpd.conf
 
 #PHP
-sudo apt-get install -y php5 libapache2-mod-php5 php5-intl php5-mcrypt php5-curl php5-gd php5-sqlite
+sudo apt-get install -y php5 libapache2-mod-php5 php5-intl php5-mcrypt php5-curl php5-gd php5-sqlite php-pear
 
 #MySQL
 sudo apt-get install -y mysql-server mysql-client php5-mysql
