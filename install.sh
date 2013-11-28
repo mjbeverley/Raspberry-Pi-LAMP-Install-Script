d9f67fbe74fe084667707fc601ba4437c7d593cc
mjbeverley <mjbeverley@gmail.com>
2013-11-28 22:39:52 +1100

    Update install.sh

--- install.sh
+++ install.sh
@@ -21,7 +21,7 @@ sudo apt-get install -y apache2
 sudo echo "ServerName localhost" >> /etc/apache2/httpd.conf
 
 #PHP
-sudo apt-get install -y php5 libapache2-mod-php5 php5-intl php5-mcrypt php5-curl php5-gd php5-sqlite php-pear
+sudo apt-get install -y php5 libapache2-mod-php5 php5-intl php5-mcrypt php5-curl php5-gd php5-sqlite php-pear php-apc
 
 #MySQL
 sudo apt-get install -y mysql-server mysql-client php5-mysql
