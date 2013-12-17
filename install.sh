558253b3b050dae5510e6b01c6cf9a7c63cc7250
mjbeverley <mjbeverley@gmail.com>
2013-12-17 23:54:09 +1100

    Update install.sh

--- install.sh
+++ install.sh
@@ -18,13 +18,16 @@ sudo apt-get install -y proftpd
 
 #Apache
 sudo apt-get install -y apache2
-sudo echo "ServerName localhost" >> /etc/apache2/httpd.conf
+sudo echo "ServerName localhost" >> /etc/apache2/apache2.conf
 
 #PHP
 sudo apt-get install -y php5 libapache2-mod-php5 php5-intl php5-mcrypt php5-curl php5-gd php5-sqlite php-pear php-apc
 
 #MySQL
 sudo apt-get install -y mysql-server mysql-client php5-mysql
+
+#phpMyAdmin
+sudo apt-get install -y phpmyadmin
 
 #Additional Dependencies
 #sudo apt-get install -y nmap zenmap
