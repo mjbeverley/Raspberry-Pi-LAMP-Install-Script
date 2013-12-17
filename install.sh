29ff4ef1800e52971b813ed5d8e0afdc44d26e01
mjbeverley <mjbeverley@gmail.com>
2013-12-18 06:36:27 +1100

    Update install.sh

--- install.sh
+++ install.sh
@@ -33,11 +33,8 @@ sudo apt-get install -y phpmyadmin
 #sudo apt-get install -y nmap zenmap
 
 #With apache2 and php5 installed you need to make three changes in the file
-
 #sudo gedit /etc/php5/apache2/php.ini
-
 #Search for entry post_max_size = 2M where you need to enter a larger number than the size of your database (15M in this case) i.e. 25M, next edit memory_limit and give a larger number than the one given to post_max_size. At last, edit the entry upload_max_filesize with a value smaller than post_max_size.
-
 #Starting from bigger values the order should go like this
 
 #    memory_limit
@@ -45,7 +42,6 @@ sudo apt-get install -y phpmyadmin
 #    upload_max_filesize
 
 #Restart apache (for ex: sudo /etc/init.d/apache2 restart) and you are set.
-
 #Don't forget to Restart Apache Services for changes to get applied
 
 
